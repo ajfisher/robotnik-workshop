18bddb6f11100aa17019f1b6d0767e1dd39b9e7b
ajfisher <ajfisher.td@gmail.com>
2016-06-11 21:38:42 +1000

    cleanup

--- utils/build-docs.sh
+++ utils/build-docs.sh
@@ -5,10 +5,7 @@
 # run from project root as utils/build-docs.sh
 
 cd exercises
-#pandoc -s -S --toc --highlight-style pygments README.md led.md servo.md motor.md robot.md bluetooth.md -o README.pdf
-
 pandoc -s -S --highlight-style pygments -t html5 --toc --toc-depth=2  -c ../css/styles.css -o README.html led.md servo.md motor.md robot.md bluetooth.md
 
 cd ..
-
 pandoc -s -S --highlight-style pygments -t html5 --toc -c css/styles.css -o README.html README.md
